277f5bc8e6b82eeba6fa8880c291cc7733cf1caf
nattyu <46308575+nattyu@users.noreply.github.com>
2023-04-12 21:04:04 +0900

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$host = "mysql209.phy.lolipop.lan";
+$dbName = "LAA1470963-jikanwari";
+$user = "LAA1470963";
+$password = "LZ69qr42mUP7Ecc";
+$dsn = "mysql:host={$host};dbname={$dbName};charser=utf8";
+?>
