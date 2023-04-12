d78c0a7dfc49b830fcb710f5a2915a16af39d945
nattyu <46308575+nattyu@users.noreply.github.com>
2023-04-12 21:03:34 +0900

    Add files via upload

---
+++ 04_task_manage/task_manager.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../header.css">
+    <link rel="stylesheet" href="../practices_Bootstrap/css/bootstrap.min.css">
+    <title>タスク管理画面</title>
+</head>
+<body>
+    <?php include('../practices_Bootstrap/index.html'); ?>
+    <main class="main-contents">
+        <p>このページでタスク管理が出来ます</p>
+    </main>
+    <script src="../practices_Bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
